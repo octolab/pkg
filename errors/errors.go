929a1292ce48dc97180eb747d893c3a000af3605
Kamil Samigullin <kamil@samigullin.info>
2020-04-02 23:39:14 +0300

    add retriable error interface

--- errors/errors.go
+++ errors/errors.go
@@ -21,3 +21,42 @@ type Message string
 func (err Message) Error() string {
 	return string(err)
 }
+
+// Retriable represents a retriable error.
+//
+// It is compatible with github.com/kamilsk/retry (v4 and later).
+type Retriable interface {
+	error
+	Retriable() bool // Is the error retriable?
+}
+
+// Unwrap returns the result of calling the Unwrap or Cause methods
+// on the error, otherwise it returns error itself.
+//
+// It is compatible with github.com/pkg/errors
+// and built-in errors since 1.13.
+func Unwrap(err error) error {
+	// compatible with github.com/pkg/errors
+	type causer interface {
+		Cause() error
+	}
+	// compatible with built-in errors since 1.13
+	type wrapper interface {
+		Unwrap() error
+	}
+
+	for err != nil {
+		layer, is := err.(wrapper)
+		if is {
+			err = layer.Unwrap()
+			continue
+		}
+		cause, is := err.(causer)
+		if is {
+			err = cause.Cause()
+			continue
+		}
+		break
+	}
+	return err
+}
--- errors/errors_test.go
+++ errors/errors_test.go
@@ -1,15 +1,20 @@
 package errors_test
 
 import (
+	stderrors "errors"
+	"fmt"
 	"testing"
 
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
+
 	. "go.octolab.org/errors"
+	"go.octolab.org/runtime"
 )
 
 const expected Message = "test"
 
-func TestMessage_Error(t *testing.T) {
+func TestMessage(t *testing.T) {
 	caller := func() error {
 		return Message("test")
 	}
@@ -17,3 +22,34 @@ func TestMessage_Error(t *testing.T) {
 	assert.EqualError(t, caller(), "test")
 	assert.True(t, expected == caller())
 }
+
+func TestUnwrap(t *testing.T) {
+	t.Run("compatible with github.com/pkg/errors", func(t *testing.T) {
+		cause := stderrors.New("origin")
+		err := errors.WithMessage(cause, "wrapper")
+		assert.NotEqual(t, cause, err)
+		assert.Equal(t, cause, Unwrap(err))
+	})
+	t.Run("compatible with built-in errors", func(t *testing.T) {
+		version := runtime.Version()
+		if version.Minor < 13 {
+			t.SkipNow()
+		}
+		cause := stderrors.New("origin")
+		err := fmt.Errorf("wrapper: %w", cause)
+		assert.NotEqual(t, cause, err)
+		assert.Equal(t, cause, Unwrap(err))
+	})
+	t.Run("onion", func(t *testing.T) {
+		cause := stderrors.New("origin")
+		err := fmt.Errorf("wrapper: %w", errors.WithMessage(layer{cause}, "wrapper"))
+		assert.NotEqual(t, cause, err)
+		assert.Equal(t, cause, Unwrap(err))
+	})
+}
+
+// helpers
+
+type layer struct{ error }
+
+func (onion layer) Cause() error { return onion.error }
