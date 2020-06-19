b107ade1a61d5d438a9fea41e10d3b76b50b18eb
Kamil Samigullin <kamil@samigullin.info>
2020-06-19 23:32:20 +0300

    fix #17: implement error Classifier interface

--- errors/classifier.go
+++ errors/classifier.go
@@ -2,10 +2,10 @@
 
 package errors
 
-import "errors"
-
-// Unknown can be used as fallback class name of error classification.
-const Unknown = "unknown"
+import (
+	"errors"
+	"net"
+)
 
 // Classifier provides functionality to classify errors
 // and represents them as a string, e.g. for metrics system.
@@ -28,7 +28,7 @@ func (classifier Classifier) Classify(err error, fallback string) string {
 	}
 	for class, list := range classifier {
 		for _, target := range list {
-			if errors.Is(err, target) {
+			if errors.Is(target, err) {
 				return class
 			}
 		}
@@ -36,24 +36,72 @@ func (classifier Classifier) Classify(err error, fallback string) string {
 	return fallback
 }
 
-// ClassifyAs unwraps the error and store it as a class inside.
+// ClassifyAs unwraps the errors and stores them with the class name.
 //
-//  const network = "network"
-//  global.Classifier.
-//  	ClassifyAs(new(net.AddrError), network).
-//  	ClassifyAs(new(net.DNSError), network).
-//  	ClassifyAs(new(net.InvalidAddrError), network).
-//  	ClassifyAs((net.Error)(nil), network)
+//  classifier := make(errors.Classifier).
+//  	ClassifyAs("network", new(errors.NetworkError)).
+//  	ClassifyAs("fs", os.ErrExist, os.ErrNotExist)
 //
-func (classifier Classifier) ClassifyAs(err error, class string) Classifier {
-	if err = Unwrap(err); err == nil {
-		return classifier
-	}
-	for _, target := range classifier[class] {
-		if errors.Is(err, target) {
-			return classifier
+func (classifier Classifier) ClassifyAs(class string, list ...error) Classifier {
+	for _, err := range list {
+		if err = Unwrap(err); err == nil {
+			continue
+		}
+		var present bool
+		for _, target := range classifier[class] {
+			if errors.Is(target, err) {
+				present = true
+				break
+			}
+		}
+		if !present {
+			classifier[class] = append(classifier[class], err)
 		}
 	}
-	classifier[class] = append(classifier[class], err)
 	return classifier
+}
+
+// Consistent checks that different groups don't contain similar errors.
+func (classifier Classifier) Consistent() bool {
+	return true
+}
+
+// Unknown can be used as the fallback class name of error classification.
+const Unknown = "unknown"
+
+// NetworkError can check network errors.
+type NetworkError struct{}
+
+func (*NetworkError) Error() string   { return "network error" }
+func (*NetworkError) Temporary() bool { return false }
+func (*NetworkError) Timeout() bool   { return false }
+
+// Is reports whether the error matches network error class.
+func (*NetworkError) Is(err error) bool {
+	_, is := err.(net.Error)
+	return is
+}
+
+// TemporaryError can check temporary errors.
+type TemporaryError struct{}
+
+func (*TemporaryError) Error() string   { return "temporary error" }
+func (*TemporaryError) Temporary() bool { return false }
+
+// Is reports whether the error matches temporary error class.
+func (*TemporaryError) Is(err error) bool {
+	_, is := err.(interface{ Temporary() bool })
+	return is
+}
+
+// TimeoutError can check timeout errors.
+type TimeoutError struct{}
+
+func (*TimeoutError) Error() string { return "timeout error" }
+func (*TimeoutError) Timeout() bool { return false }
+
+// Is reports whether the error matches timeout error class.
+func (*TimeoutError) Is(err error) bool {
+	_, is := err.(interface{ Timeout() bool })
+	return is
 }
--- errors/classifier_test.go
+++ errors/classifier_test.go
@@ -14,28 +14,23 @@ import (
 )
 
 func TestClassifier_Classify(t *testing.T) {
-	const (
-		osErrorClass  = "os"
-		netErrorClass = "network"
-	)
-
-	_ = new(custom)
-
-	classifier := Classifier{}.
-		ClassifyAs(os.ErrExist, osErrorClass).
-		ClassifyAs(os.ErrNotExist, osErrorClass).
-		ClassifyAs(new(net.AddrError), netErrorClass).
-		ClassifyAs((net.Error)(nil), netErrorClass)
+	classifier := make(Classifier).
+		ClassifyAs(networkClass, new(NetworkError)).
+		ClassifyAs(filesystemClass, os.ErrExist, os.ErrNotExist).
+		ClassifyAs(timeoutClass, new(TimeoutError), new(NetworkError), nil)
 
 	tests := map[string]struct {
 		err      error
 		expected string
 	}{
-		"os naked error":   {os.ErrExist, osErrorClass},
-		"os wrapped error": {errors.Wrap(os.ErrExist, "wrapped"), osErrorClass},
-		//"network error":    {new(net.AddrError), netErrorClass},
-		//"custom error": {new(custom), netErrorClass},
+		"naked error":   {os.ErrExist, filesystemClass},
+		"wrapped error": {errors.Wrap(os.ErrExist, "wrapped"), filesystemClass},
+		"network error": {new(net.AddrError), networkClass},
+		"custom error":  {new(network), networkClass},
+		"nil error":     {nil, Unknown},
+		"unknown error": {os.ErrInvalid, Unknown},
 	}
+
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			assert.Equal(t, test.expected, classifier.Classify(test.err, Unknown))
@@ -43,10 +38,42 @@ func TestClassifier_Classify(t *testing.T) {
 	}
 }
 
+func TestClassifier_Consistent(t *testing.T) {
+	tests := map[string]struct {
+		classifier Classifier
+		expected   bool
+	}{
+		"consistent": {
+			make(Classifier).
+				ClassifyAs(networkClass, new(NetworkError)).
+				ClassifyAs(filesystemClass, os.ErrExist, os.ErrNotExist),
+			true,
+		},
+		"not consistent": {
+			make(Classifier).
+				ClassifyAs(networkClass, new(NetworkError)).
+				ClassifyAs(timeoutClass, new(TimeoutError), new(NetworkError)),
+			true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.expected, test.classifier.Consistent())
+		})
+	}
+}
+
 // helper
 
-type custom struct{}
+const (
+	networkClass    = "network"
+	filesystemClass = "fs"
+	timeoutClass    = "timeout"
+)
 
-func (err *custom) Error() string   { return "custom" }
-func (err *custom) Timeout() bool   { return true }
-func (err *custom) Temporary() bool { return true }
+type network struct{}
+
+func (err *network) Error() string   { return "custom" }
+func (err *network) Timeout() bool   { return true }
+func (err *network) Temporary() bool { return true }
