e5f2ff9b02160c702e9b218a07fe295e828f3884
Kamil Samigullin <kamil@samigullin.info>
2020-09-14 08:35:28 +0300

    fix cascadeCloser for writer

--- io/closer.go
+++ io/closer.go
@@ -18,11 +18,16 @@ type cascadeCloser struct {
 
 // Close implements io.Closer interface and calls a Close method on the chain.
 func (closer cascadeCloser) Close() error {
+	if closer.WriteCloser != nil {
+		if err := closer.WriteCloser.Close(); err != nil {
+			return err
+		}
+	}
 	if err := closer.previous.Close(); err != nil {
 		return err
 	}
 	if closer.ReadCloser != nil {
 		return closer.ReadCloser.Close()
 	}
-	return closer.WriteCloser.Close()
+	return nil
 }
