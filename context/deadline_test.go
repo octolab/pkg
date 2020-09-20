c5ca118b401cbc4d2ad9d1fbeaf7e3d7e2804aa9
Kamil Samigullin <kamil@samigullin.info>
2020-09-20 19:39:54 +0000

    fix #24: add aliases for time, cover context package

---
+++ context/deadline_test.go
@@ -0,0 +1,36 @@
+package context_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	. "go.octolab.org/context"
+	"go.octolab.org/time"
+)
+
+func TestWithDeadline(t *testing.T) {
+	root, cancel := context.WithTimeout(context.TODO(), 100*time.Millisecond)
+	defer cancel()
+
+	ctx, cancel := WithDeadline(context.TODO(), root, 50*time.Millisecond)
+	defer cancel()
+
+	infinite, cancel := WithDeadline(context.TODO(), context.TODO(), 10*time.Millisecond)
+	defer cancel()
+
+	test, cancel := WithDeadline(infinite, t, time.Millisecond)
+	defer cancel()
+
+	select {
+	case <-root.Done():
+		require.Fail(t, "root is not expected")
+	case <-infinite.Done():
+		require.Fail(t, "infinite is not expected")
+	case <-test.Done():
+		require.Fail(t, "test is not expected")
+	case <-ctx.Done():
+		// success
+	}
+}
--- time/time.go
+++ time/time.go
@@ -3,6 +3,13 @@ package time
 import "time"
 
 const (
+	Nanosecond  = time.Nanosecond
+	Microsecond = time.Microsecond
+	Millisecond = time.Millisecond
+	Second      = time.Second
+	Minute      = time.Minute
+	Hour        = time.Hour
+
 	Day  = 24 * time.Hour
 	Week = 7 * Day
 )
