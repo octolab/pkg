42afd9d1de0029415bd4bc1aa997c569ccbc7f00
Kamil Samigullin <kamil@samigullin.info>
2021-02-17 09:25:57 +0300

    fix #42: add unsafe.Return function

--- unsafe/suppress.go
+++ unsafe/suppress.go
@@ -17,3 +17,15 @@ func DoSilent(interface{}, error) {}
 //  )
 //
 func Ignore(error) {}
+
+// Return accepts a result with an error
+// and returns the first to cast it later.
+//
+//  import (
+//  	"github.com/Masterminds/semver"
+//  	"go.octolab.org/unsafe"
+//  )
+//
+//  var min := unsafe.Return(semver.NewConstraint(">= 92.0")).(*semver.Constraints)
+//
+func Return(in interface{}, _ error) interface{} { return in }
