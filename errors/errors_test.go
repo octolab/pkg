d8517c0f3c4d86584cf71bf8bdae434cfcefc4fc
Kamil Samigullin <kamil@samigullin.info>
2021-04-23 16:01:45 +0300

    add proxy call for errors

--- errors/errors_test.go
+++ errors/errors_test.go
@@ -2,6 +2,7 @@ package errors_test
 
 import (
 	stderrors "errors"
+	"fmt"
 	"testing"
 
 	"github.com/pkg/errors"
@@ -22,8 +23,30 @@ func TestMessage(t *testing.T) {
 }
 
 func TestUnwrap(t *testing.T) {
-	cause := stderrors.New("origin")
-	err := errors.WithMessage(cause, "wrapper")
-	assert.NotEqual(t, cause, err)
-	assert.Equal(t, cause, Unwrap(err))
+	t.Run("simple", func(t *testing.T) {
+		cause := stderrors.New("origin")
+		err := errors.WithMessage(cause, "wrapper")
+		assert.NotEqual(t, cause, err)
+		assert.Equal(t, cause, Unwrap(err))
+	})
+
+	t.Run("compatible with built-in errors", func(t *testing.T) {
+		cause := stderrors.New("origin")
+		err := fmt.Errorf("wrapper: %w", cause)
+		assert.NotEqual(t, cause, err)
+		assert.Equal(t, cause, Unwrap(err))
+	})
+
+	t.Run("onion", func(t *testing.T) {
+		cause := stderrors.New("origin")
+		err := fmt.Errorf("wrapper: %w", errors.WithMessage(layer{cause}, "wrapper"))
+		assert.NotEqual(t, cause, err)
+		assert.Equal(t, cause, Unwrap(err))
+	})
 }
+
+// helpers
+
+type layer struct{ error }
+
+func (onion layer) Cause() error { return onion.error }
---
+++ errors/proxy.go
@@ -0,0 +1,38 @@
+package errors
+
+import stderrors "errors"
+
+// As finds the first error in err's chain that matches target, and if so, sets
+// target to that error value and returns true. Otherwise, it returns false.
+//
+// The chain consists of err itself followed by the sequence of errors obtained by
+// repeatedly calling Unwrap.
+//
+// An error matches target if the error's concrete value is assignable to the value
+// pointed to by target, or if the error has a method As(interface{}) bool such that
+// As(target) returns true. In the latter case, the As method is responsible for
+// setting target.
+//
+// An error type might provide an As method so it can be treated as if it were a
+// different error type.
+//
+// As panics if target is not a non-nil pointer to either a type that implements
+// error, or to any interface type.
+func As(err error, target interface{}) bool { return stderrors.As(err, target) }
+
+// Is reports whether any error in err's chain matches target.
+//
+// The chain consists of err itself followed by the sequence of errors obtained by
+// repeatedly calling Unwrap.
+//
+// An error is considered to match a target if it is equal to that target or if
+// it implements a method Is(error) bool such that Is(target) returns true.
+//
+// An error type might provide an Is method so it can be treated as equivalent
+// to an existing error. For example, if MyError defines
+//
+//	func (m MyError) Is(target error) bool { return target == fs.ErrExist }
+//
+// then Is(MyError{}, fs.ErrExist) returns true. See syscall.Errno.Is for
+// an example in the standard library.
+func Is(err, target error) bool { return stderrors.Is(err, target) }
